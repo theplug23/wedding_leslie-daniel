4e4f301281c54c5ad11a12822e8c1ad55db23e3d
theplug23 <travauxtheplug@gmail.com>
2024-03-06 17:09:29 +0100

    update

--- habibi/mail-contact.php
+++ habibi/mail-contact.php
@@ -1,7 +1,9 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['send'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $cc = 'infinityparty237@gmail.com,travauxtheplug@gmail.com'; // Copie du mail
+    $bcc = 'yehielyanou@gmail.com'; // Copie cachee du mail 
     $attendance = isset($_POST['radio-group']) ? $_POST['radio-group'] : '';
     $guests = $_POST['guest'];
     $event = $_POST['what'];
@@ -9,14 +11,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $to = "andytchouta@gmail.com"; // Remplacez cette adresse e-mail par votre propre adresse e-mail
     $subject = "RSVP pour un événement";
-    $message = "Nom: $name\n";
-    $message .= "Email: $email\n";
-    $message .= "Participation: $attendance\n";
-    $message .= "Nombre d'invités: $guests\n";
-    $message .= "Événement: $event\n";
-    $message .= "Préférence de repas: $meal_preference\n";
+    $message = "Nom: $name<br/>";
+    $message .= "Email: $email<br/>";
+    $message .= "Participation: $attendance<br/>";
+    $message .= "Nombre d'invités: $guests<br/>";
+    $message .= "Événement: $event<br/>";
+    $message .= "Préférence de repas: $meal_preference<br/>";
 
     $headers = array(
+        'From' => ''.$email.'',
+        'Cc' => ''.$cc.'',
+        'Bcc' => ''.$bcc.'',
         'Content-type' => 'text/html; charset=utf-8',
         'MIME-version' => '1.0',
         'Reply-To' => ''.$email.''
@@ -26,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // ini_set("smtp_port", "1025");
     $true = mail($to, $subject, $message, $headers);
     if ($true) {
-        echo "<p style='color:green;'>Merci, message envoyé</p>";
+        echo "<p style='color:green;'>Merci  message envoyé</p>";
     } else {
         echo "<p style='color:red;'>Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer.</p>";
     }
