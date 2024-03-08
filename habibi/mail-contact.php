64e3429ee216e575ee84fc63cb2d696841084e39
theplug23 <travauxtheplug@gmail.com>
2024-03-08 11:42:46 +0100

    remove cc & cci

--- habibi/mail-contact.php
+++ habibi/mail-contact.php
@@ -2,10 +2,6 @@
 if (isset($_POST['send'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    // $cc = 'infinityparty237@gmail.com,travauxtheplug@gmail.com';
-     // Copie du mail
-    // $bcc = 'yehielyanou@gmail.com'; 
-    // Copie cachee du mail 
     $attendance = isset($_POST['radio-group']) ? $_POST['radio-group'] : '';
     $guests = $_POST['guest'];
     $event = $_POST['what'];
@@ -13,25 +9,24 @@ if (isset($_POST['send'])) {
 
     $to = "andytchouta@gmail.com"; // Remplacez cette adresse e-mail par votre propre adresse e-mail
     $subject = "RSVP pour un événement";
-    $message = "Nom: $name<br/>";
-    $message .= "Email: $email<br/>";
-    $message .= "Participation: $attendance<br/>";
-    $message .= "Nombre d'invités: $guests<br/>";
-    $message .= "Événement: $event<br/>";
-    $message .= "Préférence de repas: $meal_preference<br/>";
+    $message = "Nom: $name\n";
+    $message .= "Email: $email\n";
+    $message .= "Participation: $attendance\n";
+    $message .= "Nombre d'invités: $guests\n";
+    $message .= "Événement: $event\n";
+    $message .= "Préférence de repas: $meal_preference\n";
 
     $headers = array(
-        // 'From' => ''.$email.'',
-        // 'Cc' => ''.$cc.'',
-        // 'Bcc' => ''.$bcc.'',
+        'From' => ''.$email.'',
         'Content-type' => 'text/html; charset=utf-8',
         'MIME-version' => '1.0',
         'Reply-To' => ''.$email.''
     );
 
+	
     $true = mail($to, $subject, $message, $headers);
     if ($true) {
-        echo "<p style='color:green;'>Merci message envoyé</p>";
+        echo "<p style='color:green;'>Merci  message envoyé</p>";
     } else {
         echo "<p style='color:red;'>Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer.</p>";
     }
